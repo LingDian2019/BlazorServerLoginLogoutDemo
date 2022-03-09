924ab1ce29b957f7a661e5127435bfa89418da0e
lingdian <857388137@qq.com>
2022-03-09 09:01:38 +0800

    保存

--- src/BlazorServerLoginLogoutDemo.BlazorApp/Program.cs
+++ src/BlazorServerLoginLogoutDemo.BlazorApp/Program.cs
@@ -1,13 +1,17 @@
-using BlazorServerLoginLogoutDemo.BlazorApp.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
+using Blazored.LocalStorage;
+using BlazorServerLoginLogoutDemo.Authentication;
+using Microsoft.AspNetCore.Components.Authorization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddAuthorizationCore();
+builder.Services.AddAuthenticationCore();
+builder.Services.AddBlazoredLocalStorage();
+builder.Services.AddSingleton<LoginService>();
+builder.Services.AddScoped<AuthenticationStateProvider, SystemAuthenticationStateProvider>();
 
 var app = builder.Build();
 
@@ -22,7 +26,10 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-
+//认证（Authentication）：用于认证登录权限，一般是登录账号名和密码确认的认证，保证用户认证资格。
+app.UseAuthentication();
+//授权（Authorization）：授权，比如很多后台，需要区分管理员，分级管理，这样通过授权机制，可以很好的进行权限管理。
+app.UseAuthorization();
 app.UseRouting();
 
 app.MapBlazorHub();
